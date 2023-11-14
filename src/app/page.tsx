18036228379a18212b4bfc1c6c07b0667f1fe7a2
Utkarsh Saini <sainiutkarsh01@gmail.com>
2023-11-14 23:15:53 +0530

    Added MeetAhead and SelfImprovement components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from '../components/Navbar';
 import Firstpage from '../components/Firstpage';
 import EQbeats from '../components/EQbeats';
 import DoesThisSound from '../components/DoesThisSound';
+import MeetAhead from '../components/MeetAhead';
+import SelfImprovement from '../components/SelfImprovement';
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Firstpage />
       <EQbeats />
       <DoesThisSound />
+      <MeetAhead />
+      <SelfImprovement />
       </>
   );
 }
